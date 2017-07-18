42c213d778186e9c4b62c74bc00daf1165925a17
victor.wang <victor.wang@PS00025.pongmarketing.local>
2017-07-18 11:17:19 -0400

    add

--- Deployer/src/main/java/com/yw/test1/DateUtil.java
+++ Deployer/src/main/java/com/yw/test1/DateUtil.java
@@ -21,7 +21,7 @@ public class DateUtil {
     // Add your own format here.
     private static List<SimpleDateFormat> 
             dateFormats = new ArrayList<SimpleDateFormat>() {
-		private static final long serialVersionUID = 111100011L; 
+		private static final long serialVersionUID = 111100000011L; 
 		{
             add(new SimpleDateFormat("M/dd/yyyy"));
             add(new SimpleDateFormat("dd.M.yyyy"));
